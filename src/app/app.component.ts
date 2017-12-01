16f9641421b21c7b2a9145f4f0bb0ef251c031ba
janson <hao.chen@zeiss.com>
2017-12-01 11:18:42 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,18 @@
 /**
  * Angular 2 decorators and services
  */
-import {
-  Component,
-  OnInit,
-  ViewEncapsulation
-} from '@angular/core';
-import { AppState } from './app.service';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { AppState } from "./app.service";
 
 /**
  * App Component
  * Top Level Component
  */
 @Component({
-  selector: 'app',
-  encapsulation: ViewEncapsulation.None,
-  styleUrls: [
-    './app.component.css'
-  ],
-  template: `
+    selector: "app",
+    encapsulation: ViewEncapsulation.None,
+    styleUrls: ["./app.component.css"],
+    template: `
     <nav>
       <a [routerLink]=" ['./'] "
         routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
@@ -48,18 +42,15 @@ import { AppState } from './app.service';
   `
 })
 export class AppComponent implements OnInit {
-  public angularclassLogo = 'assets/img/angularclass-avatar.png';
-  public name = 'Angular 2 Webpack Starter';
-  public url = 'https://twitter.com/AngularClass';
+    public angularclassLogo = "assets/img/angularclass-avatar.png";
+    public name = "Angular 2 Webpack Starter";
+    public url = "https://twitter.com/AngularClass";
 
-  constructor(
-    public appState: AppState
-  ) {}
+    constructor(public appState: AppState) {}
 
-  public ngOnInit() {
-    console.log('Initial App State', this.appState.state);
-  }
-
+    public ngOnInit() {
+        console.log("Initial App State", this.appState.state);
+    }
 }
 
 /**
