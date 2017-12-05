2b5623527606a578f61f7c5ce2312291579ac8cc
janson <hao.chen@zeiss.com>
2017-12-05 20:47:08 +0800

    test hook

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,66 +1,62 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import {
-  inject,
-  async,
-  TestBed,
-  ComponentFixture
-} from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { inject, async, TestBed, ComponentFixture } from "@angular/core/testing";
 
 /**
  * Load the implementations that should be tested
  */
-import { AppComponent } from './app.component';
-import { AppState } from './app.service';
+import { AppComponent } from "./app.component";
+import { AppState } from "./app.service";
 
 describe(`App`, () => {
-  let comp: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
-
-  /**
-   * async beforeEach
-   */
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppComponent ],
-      schemas: [NO_ERRORS_SCHEMA],
-      providers: [AppState]
-    })
-    /**
-     * Compile template and css
-     */
-    .compileComponents();
-  }));
-
-  /**
-   * Synchronous beforeEach
-   */
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    comp    = fixture.componentInstance;
+    let comp: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
 
     /**
-     * Trigger initial data binding
+     * async beforeEach
      */
-    fixture.detectChanges();
-  });
+    beforeEach(
+        async(() => {
+            TestBed.configureTestingModule({
+                declarations: [AppComponent],
+                schemas: [NO_ERRORS_SCHEMA],
+                providers: [AppState]
+            })
+                /**
+                 * Compile template and css
+                 */
+                .compileComponents();
+        })
+    );
 
-  it(`should be readly initialized`, () => {
-    expect(fixture).toBeDefined();
-    expect(comp).toBeDefined();
-  });
+    /**
+     * Synchronous beforeEach
+     */
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        comp = fixture.componentInstance;
 
-  it(`should be @TipeIO`, () => {
-    expect(comp.twitter).toEqual('https://twitter.com/gdi2290');
-    expect(comp.tipe).toEqual('assets/img/tipe.png');
-    expect(comp.name).toEqual('Angular Starter');
-  });
+        /**
+         * Trigger initial data binding
+         */
+        fixture.detectChanges();
+    });
 
-  it('should log ngOnInit', () => {
-    spyOn(console, 'log');
-    expect(console.log).not.toHaveBeenCalled();
+    it(`should be readly initialized`, () => {
+        expect(fixture).toBeDefined();
+        expect(comp).toBeDefined();
+    });
 
-    comp.ngOnInit();
-    expect(console.log).toHaveBeenCalled();
-  });
+    it(`should be @TipeIO`, () => {
+        expect(comp.twitter).toEqual("https://twitter.com/gdi2290");
+        expect(comp.tipe).toEqual("assets/img/tipe.png");
+        expect(comp.name).toEqual("Angular Starter");
+    });
 
+    it("should log ngOnInit", () => {
+        spyOn(console, "log");
+        expect(console.log).not.toHaveBeenCalled();
+
+        comp.ngOnInit();
+        expect(console.log).toHaveBeenCalled();
+    });
 });
